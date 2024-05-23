55a64693c218911a38e8c4f09b5bba61e305c412
ShepherdBoyy <shepherd052603@gmail.com>
2024-05-23 16:26:36 +0800

    Third Commit

--- utils/db.js
+++ utils/db.js
@@ -1,10 +1,13 @@
 import mysql from "mysql"
 
+require("dotenv").config()
+const mysql = require("mysql2")
+
 const con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "barangay_database",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 })
 
 con.connect(function (err) {
