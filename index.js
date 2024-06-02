2adbba717c6096c8c4c7610327558c3e8a3cf1fe
ShepherdBoyy <shepherd052603@gmail.com>
2024-06-03 02:41:31 +0800

    Finish Error

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { adminRouter } from "./routes/AdminRoute.js"
 const app = express()
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://shepherdboyy.github.io"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
